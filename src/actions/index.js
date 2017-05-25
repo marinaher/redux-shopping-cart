fe4de26a35a81b04ad20ef1b2eefab22bbf718e7
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-05-25 11:46:26 -0500

    added removeFromCart action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
 export const addToCart = (item) => ({
   type: ADD_TO_CART,
   payload: item,
-})
+});
 
 // addToCart is taking in an item and returning an object that has a type and payload
+
+export const removeFromCart = (id) => ({
+  type: REMOVE_FROM_CART,
+  payload: id
+});
+
+// removeFromCart is taking in the id of an item and then filters the existing shopping cart and removes the item
